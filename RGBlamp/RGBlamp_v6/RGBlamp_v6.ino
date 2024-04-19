48ace4c7b565655bea01b899a3b5527e64128567
Jiri Vintera <jvintera@spselitdobruska.cz>
2024-04-19 10:02:56 +0200

    Plně funkční přes Arduino cloud, zpoždění přes delay

--- RGBlamp/RGBlamp_v6/RGBlamp_v6.ino
+++ RGBlamp/RGBlamp_v6/RGBlamp_v6.ino
@@ -1,185 +1,240 @@
+// Opravit rozsvěcení LED
+// Když stisknu HW tlačítko, změní se stav sw tlačítka bez ohledu na wifi?
+// Jak si to testuje wifi?
+// Popsat v nápovědě soubor wifiConfig.h
+
+
+
 //***************************************************
 // Externí LED WB2812B přepínané tlačítkem a přes Blynk
 // Ošetřeno: manuální ovládání i když není spojení přes wi-fi
 // Efekt Rainbow
 //***************************************************
 /*Zajímavé odkazy
-  Původní program
-  je příklad z https://docs.m5stack.com/en/unit/neopixel
-  resp. https://github.com/m5stack/M5Stack/blob/master/examples/Unit/RGB_LED_SK6812/display_rainbow/display_rainbow.ino
-  Následně předělán program podle příkladu 
+  Arduino IoT cloud https://youtu.be/rcCxGcRwCVk?feature=shared
+      https://docs.arduino.cc/arduino-cloud/guides/overview/
+  Návod https://youtu.be/rcCxGcRwCVk?feature=shared
 
-  Knihovna a obecné informace
-  https://docs.m5stack.com/en/unit/neopixel
-  https://github.com/FastLED/FastLED/wiki/Overview
-  https://github.com/FastLED/FastLED/wiki/Pixel-reference#chsv
-  https://cs.wikipedia.org/wiki/HSV
-  Příklady
-  https://wokwi.com/arduino/libraries/FastLED/Blink
-  BLYNK IoT
-  https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk
-  Příklad pro tlačítko https://examples.blynk.cc/?board=ESP32&shield=ESP32%20WiFi&example=GettingStarted%2FVirtualPinRead&auth=_l5IbMrtHpyQaAeHqG1wCQXiu--KYDOA&dev=Quickstart%20Device&tmpl=TMPLIXw3EzCx
-  Příklad pro LED https://examples.blynk.cc/?board=ESP32&shield=ESP32%20WiFi&example=GettingStarted%2FVirtualPinWrite&auth=_l5IbMrtHpyQaAeHqG1wCQXiu--KYDOA&dev=Quickstart%20Device&tmpl=TMPLIXw3EzCx
-  Příklady pro wi-fi https://randomnerdtutorials.com/esp32-useful-wi-fi-functions-arduino/
 */
 
-//Připojení knihoven
 
-
-
-
-
-
-/*
-#include "M5Atom.h"
-#include "FastLED.h"
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
-
-//Údaje Blynk
-// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
-// See the Device Info tab, or Template settings
-#define BLYNK_TEMPLATE_ID "TMPLQwu5D1qI"
-#define BLYNK_DEVICE_NAME "HEX light"
-#define BLYNK_AUTH_TOKEN "n2AK1jAdi-jdgCZ0U4V1FFFHo2ysTxrg"
-// Comment this out to disable prints and save space
-#define BLYNK_PRINT Serial
-char auth[] = BLYNK_AUTH_TOKEN;
-// Your WiFi credentials.
-// Set password to "" for open networks.
-//char ssid[] = "MPT_BASTL";
-//char pass[] = "MPT4Bastleni";
-char ssid[] = "Zima";
-char pass[] = "Ro2uJjej";
-char server[] = "blynk.cloud";
-int port = 80;
-BlynkTimer timer; // Creating a timer object
-bool BlynkConnected;
-
-//LED pasek/modul WS2812B
-#define Neopixel_PIN 26
-#define NUM_LEDS 24
-CRGB leds[NUM_LEDS];
-uint8_t gHue = 0;
-int LED_brightness = 5;
-
-//Integrovaná LED v M5Atom
-#define Neopixel_PIN2 27
-#define NUM_LEDS2 1
-CRGB leds2[NUM_LEDS2];
-int LED2_brightness = 20;
-
-//Tlačítko
-#define BTN_PIN 25
-bool stateB = 1; //Pomocná proměnná pro přepínání stavu LED
-int BTN_last = 0; //Proměnná pro uložení předešlého stavu tlačítka
-int BTN_cur = 0; //Proměnná pro uložení současného stavu tlačítka
-int BTN_SW_last = 0; //Proměnná v Blynku pro uložení předešlého stavu tlačítka
-int BTN_SW_cur = 0; //Proměnná v Blynku pro uložení současného stavu tlačítka
+#include "thingProperties.h"
 
 void setup()
 {
-  M5.begin();
+  // Initialize serial and wait for port to open:
   Serial.begin(115200);
+  // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
+//  delay(1500);
 
-  pinMode(BTN_PIN, INPUT_PULLUP); //Nastavení tlačítka - vnitřní Pull-up rezistor
-  // Neopixel initialization
-  FastLED.addLeds<WS2812B, Neopixel_PIN2, GRB>(leds2, NUM_LEDS2).setCorrection(TypicalLEDStrip);
-  FastLED.setBrightness(LED2_brightness);
-  FastLED.addLeds<WS2812B, Neopixel_PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  FastLED.setBrightness(LED_brightness);
-  //Na začátku se LED zhasnou
-  fill_solid(leds, NUM_LEDS, CRGB(0, 0, 0)); //LED pásek zhasne
-  fill_solid(leds2, NUM_LEDS2, CRGB(0, 0, 255)); //LED se rozsvítí modře a měla by po 50 ms změnit barvu
-  FastLED.show();
+  // Defined in thingProperties.h
+  initProperties();
 
-  // Blynk.begin(auth, ssid, pass);
-  //  You can also specify server:
-  // Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
-  // Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
-  // WIFI_STA viz https://randomnerdtutorials.com/esp32-useful-wi-fi-functions-arduino/
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, pass);
-  Serial.print("Connecting to WiFi...");
-  for (byte i = 0; (i < 10) && (WiFi.status() != WL_CONNECTED); i++)
-  {
-    Serial.print('.');
-    delay(1000);
-  }
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    Serial.println(WiFi.localIP());
-    Blynk.config(auth, server, port);
-  }
-  else 
-  {
-    fill_solid(leds2, NUM_LEDS2, CRGB(255, 255, 0)); // LED se rozsvítí modře a měla by po 50 ms změnit barvu
-    FastLED.show();
-    WiFi.disconnect();
-  }
-  // Blynk.connected();
+  // Connect to Arduino IoT Cloud
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
 
-  if (WiFi.status() == WL_CONNECTED)
-  {
-      timer.setInterval(500L, myTimerEvent); // Staring a timer
-  }
-  delay(50);
-}
+  /*
+     The following function allows you to obtain more information
+     related to the state of network and IoT Cloud connection and errors
+     the higher number the more granular information you’ll get.
+     The default is 0 (only errors).
+     Maximum is 4
+ */
+  setDebugMessageLevel(2);
+  ArduinoCloud.printDebugInfo();
 
-void myTimerEvent() // This loop defines what happens when timer is triggered
-{
-  Blynk.virtualWrite(V1, stateB);
-  Serial.print("Hodnota posuvníku V1 je: ");
-  Serial.println(stateB);
-}
-
-// Tato funkce bude volána při každém posuvníku Widget
-// v aplikaci Blynk zapíše hodnoty do virtuálního pinu 0
-BLYNK_WRITE(V0)
-{
-  int pinValue = param.asInt(); // přiřazení příchozí hodnoty z pinu V1 do proměnné
-  BTN_SW_cur = pinValue;
-  // Můžete také použít:
-  // Řetězec i = param.asStr();
-  // double d = param.asDouble();
-  Serial.print("Hodnota posuvníku V0 je: ");
-  Serial.println(pinValue);
-}
-
-void loop() {
-  BTN_cur = digitalRead(BTN_PIN); //Čtení stavu tlačítka
-
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    Blynk.run();
-    timer.run(); // runs the timer in the loop
-  }
   
+}
 
-  //Pokud došlo k uvolnění hw tlačítka (was released),
-  //nebo k uvolnění sw tlačítka, změní se stateB
-  if (BTN_cur && !BTN_last || BTN_SW_cur && !BTN_SW_last) {
+void loop()
+{
+  ArduinoCloud.update();
+
+  BTN_cur = digitalRead(BTN_PIN); // Čtení stavu tlačítka
+
+  // Pokud došlo k uvolnění hw tlačítka (was released),
+  // nebo k přepnutí sw tlačítka, změní se stateB
+  if (BTN_cur && !BTN_last)
+  {
     stateB = !stateB;
   }
-  if (stateB) {
-    //Funkce je popsána v https://fastled.io/docs/3.1/group___colorutils.html
-    fill_rainbow(leds, NUM_LEDS, gHue, 7);  // rainbow effect
-    fill_solid(leds2, NUM_LEDS2, CRGB(0, 255, 0));
-    FastLED.show();  // must be executed for neopixel becoming effective
-    EVERY_N_MILLISECONDS(20) {
-      gHue++;
-    }
+  if (BTN_SW_cur && !BTN_SW_last)
+  {
+    stateB = 1;
   }
-  else {
-    //Funkce je popsána v https://fastled.io/docs/3.1/group___colorutils.html
-    fill_solid(leds, NUM_LEDS, CRGB(0, 0, 0));
+  if (BTN_SW_last && !BTN_SW_cur)
+  {
+    stateB = 0;
+  }
+  if (stateB && !BTN_SW_RainBow && !BTN_SW_StarrLight && !BTN_SW_PoliceBeacon && !BTN_SW_Beacon)
+  {
+    // Funkce je popsána v https://fastled.io/docs/3.1/group___colorutils.html
+    //fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));
+    fill_solid(leds, NUM_LEDS, CHSV(ledsHue, ledsSaturation, ledsValue));
+    fill_solid(leds2, NUM_LEDS2, CRGB(0, 255, 0));
+    FastLED.show();
+  }
+  else
+  {
+    // Funkce je popsána v https://fastled.io/docs/3.1/group___colorutils.html
+    fill_solid(leds, NUM_LEDS, CHSV(ledsHue, ledsSaturation, 0));
     fill_solid(leds2, NUM_LEDS2, CRGB(255, 0, 0));
     FastLED.show();
   }
+  //Režim Rainbow
+  if (!stateB && BTN_SW_RainBow && !BTN_SW_StarrLight && !BTN_SW_PoliceBeacon && !BTN_SW_Beacon)
+  {
+    // Funkce je popsána v https://fastled.io/docs/3.1/group___colorutils.html
+    fill_rainbow(leds, NUM_LEDS, gHue, 7); // rainbow effect
+    fill_solid(leds2, NUM_LEDS2, CRGB(0, 255, 0));
+    FastLED.show(); // must be executed for neopixel becoming effective
+    EVERY_N_MILLISECONDS(10)
+    {
+      gHue++;
+    }
+  }
+  // Režim Star light
+  if (!stateB && !BTN_SW_RainBow && BTN_SW_StarrLight && !BTN_SW_PoliceBeacon && !BTN_SW_Beacon)
+  {
+      for (int i = 0; i < NUM_LEDS; i++)
+      {
+        leds[i] = CRGB::Black;
+      }
+
+      int numStars = random(1, 6); // Počet hvězd, které se rozblikají najednou
+
+      for (int i = 0; i < numStars; i++)
+      {
+        int pos = random(NUM_LEDS); // Náhodná pozice pro hvězdu
+        leds[pos] = CRGB::Yellow;   // Nastaví LED na žlutou
+      }
+      FastLED.show();
+      delay(100);
+  }
+  // Režim Police beacon
+  if (!stateB && !BTN_SW_RainBow && !BTN_SW_StarrLight && BTN_SW_PoliceBeacon && !BTN_SW_Beacon)
+  {
+    for (int blink = 0; blink < 3; blink++)
+    {
+      for (int i = 0; i < 12; i++)
+      {
+        leds[i] = CRGB::Blue; // Nastaví LED na modrou
+      }
+      FastLED.show();
+      delay(50); // Zpoždění mezi jednotlivými blikáními
+
+      for (int i = 0; i < 12; i++)
+      {
+        leds[i] = CRGB::Black; // Vypne LED
+      }
+      FastLED.show();
+      delay(50); // Zpoždění mezi jednotlivými blikáními
+    }
+
+    // Blikání červených LED
+    for (int blink = 0; blink < 3; blink++)
+    {
+      for (int i = 12; i < NUM_LEDS; i++)
+      {
+        leds[i] = CRGB::Red; // Nastaví LED na červenou
+      }
+      FastLED.show();
+      delay(50); // Zpoždění mezi jednotlivými blikáními
+
+      for (int i = 12; i < NUM_LEDS; i++)
+      {
+        leds[i] = CRGB::Black; // Vypne LED
+      }
+      FastLED.show();
+      delay(50); // Zpoždění mezi jednotlivými blikáními
+    }
+  }
+
+  if (!stateB && !BTN_SW_RainBow && !BTN_SW_StarrLight && !BTN_SW_PoliceBeacon && BTN_SW_Beacon)
+  {
+    static int state = 0;
+
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+      leds[i] = CRGB::Black; // Vypne všechny LED
+    }
+
+    if (state < NUM_LEDS)
+    { // Pětice svítících LED
+      for (int i = state; i < state + 12; i++)
+      {
+        leds[i % NUM_LEDS] = CRGB::Orange; // Nastaví LED na oranžovou
+      }
+    }
+
+    FastLED.show();
+    delay(20); // Zpoždění mezi jednotlivými rotacemi
+
+    state = (state + 1) % NUM_LEDS;
+  }
+
   BTN_last = BTN_cur;
   BTN_SW_last = BTN_SW_cur;
   M5.update();
 }
 
+/*
+  Since Led is READ_WRITE variable, onLedChange() is
+  executed every time a new value is received from IoT Cloud.
 */
+void onLedChange()
+{
+  // Proměnná {"bri" : "100", "hue" : "19.319", "sat" : "100", "swi" : "true"}
+  BTN_SW_cur = led.getValue().swi;
+
+  ledsHue = map(led.getValue().hue, 0, 360, 0, 255);
+  //Serial.println(led.getValue().hue);
+  //Serial.println(ledsHue);
+  ledsSaturation = map(led.getValue().sat, 0, 100, 0, 255);
+  //Serial.println(led.getValue().sat);
+  //Serial.println(ledsSaturation);
+  ledsValue = map(led.getValue().bri, 0, 100, 0, 255);
+  //Serial.println(led.getValue().bri);
+  //Serial.println(ledsValue);
+}
+
+/*
+  Since RainBow is READ_WRITE variable, onRainBowChange() is
+  executed every time a new value is received from IoT Cloud.
+*/
+void onRainBowChange()
+{
+  BTN_SW_RainBow = rainBow;
+  Serial.println(rainBow);
+}
+
+/*
+  Since StarrLight is READ_WRITE variable, onStarrLightChange() is
+  executed every time a new value is received from IoT Cloud.
+*/
+void onStarrLightChange()
+{
+  BTN_SW_StarrLight = starrLight;
+  Serial.println(starrLight);
+}
+
+/*
+  Since PoliceBeacon is READ_WRITE variable, onPoliceBeaconChange() is
+  executed every time a new value is received from IoT Cloud.
+*/
+void onPoliceBeaconChange()
+{
+  BTN_SW_PoliceBeacon = policeBeacon;
+  Serial.println(policeBeacon);
+}
+
+/*
+  Since Beacon is READ_WRITE variable, onBeaconChange() is
+  executed every time a new value is received from IoT Cloud.
+*/
+void onBeaconChange()
+{
+  BTN_SW_Beacon = beacon;
+  Serial.println(beacon);
+}
+
