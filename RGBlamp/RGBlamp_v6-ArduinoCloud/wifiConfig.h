77983a11604634d2ddd695deb12293f48623ec2a
JVintera <j.vintera@gmail.com>
2025-03-15 06:39:09 +0100

    Verze 6 nedodělaná

---
+++ RGBlamp/RGBlamp_v6-ArduinoCloud/wifiConfig.h
@@ -0,0 +1,9 @@
+/* VYPLNTE NAZEV WIFI SITE A HESLO  */
+#define SECRET_SSID "Zima"
+#define SECRET_OPTIONAL_PASS "Ro2uJjej"
+/* Device ID and Secret Key from Arduino IoT cloud */
+#define SECRET_DEVICE_LOGIN_NAME "f1941273-afe8-4ad8-98e8-fd96fc37f906"
+#define SECRET_DEVICE_KEY "3@tGlYzqWA#vUQM1fHvkjPEib"
+
+//const char SECRET_DEVICE_LOGIN_NAME[] = "f1941273-afe8-4ad8-98e8-fd96fc37f906";
+//const char SECRET_DEVICE_KEY[] = "3@tGlYzqWA#vUQM1fHvkjPEib";
